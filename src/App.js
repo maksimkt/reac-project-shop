db91e80d514cfc66737035f99ea2f6e39a2bace8
maks <makskosar79@gmil.com>
2020-11-20 14:10:56 +0200

    create new app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="app-conatiner">
+      <header className="">
+        <a href="/">Project shop</a>
+      </header>
+      <main className="">Products</main>
+      <footer className="">test</footer>
+    </div>
+  );
+}
+
+export default App;
